30bc6d53319eabbeedd88d52d2f942983ce26f9c
Haseeb ul Hassan <haseeb.ee12@gmail.com>
2020-06-11 10:23:37 +0500

    Updated the file
    Changed the css file by increasing the height and also added temperature button to increase or decrease its value.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(1);
+  const [isMorning, setMorning] = useState(false);
+  const [temp, setTemp] = useState(22);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className= {`box ${isMorning ? 'dayLight': ''}`}>
+      <h2>The count is {count} </h2>
+      <br />  
+      <button onClick= {
+        ()=> setCount (count + 1)
+      }>
+        Update count
+      </button>
+    <h1> Good {isMorning ? 'Morning' : 'Night'}</h1>
+    <br />
+    <button onClick = {
+      () => setMorning(!isMorning)
+    }>
+      Change Day
+    </button>
+    <h3> The room temperature is {temp}</h3>
+    <br />
+    <button onClick = {
+      ()=> setTemp(temp + 1)
+    }>
+      Increase temperature
+    </button>
+    <button onClick = {
+      ()=> setTemp(temp - 1)
+    }>
+      Decrease temperature
+    </button>
+      </div>
   );
 }
 
